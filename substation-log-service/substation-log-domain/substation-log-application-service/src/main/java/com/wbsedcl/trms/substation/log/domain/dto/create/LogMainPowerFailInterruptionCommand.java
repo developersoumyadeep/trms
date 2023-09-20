b2d7a241eac78baff70ea848cc91ae9564f50419
Soumyadeep Ganguly <developewithsoumyadeep@gmail.com>
2023-09-20 17:36:34 +0530

    add LogMainPowerFailInterruptionCommand.java add @JsonFormat for endDate and endTime in LogSourceChangeOverInterruptionCommand.java

--- substation-log-service/substation-log-domain/substation-log-application-service/src/main/java/com/wbsedcl/trms/substation/log/domain/dto/create/LogMainPowerFailInterruptionCommand.java
+++ substation-log-service/substation-log-domain/substation-log-application-service/src/main/java/com/wbsedcl/trms/substation/log/domain/dto/create/LogMainPowerFailInterruptionCommand.java
@@ -1,9 +1,7 @@
 package com.wbsedcl.trms.substation.log.domain.dto.create;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.wbsedcl.trms.substation.log.domain.entity.FaultNature;
 import com.wbsedcl.trms.substation.log.domain.entity.InterruptionStatus;
-import com.wbsedcl.trms.substation.log.domain.entity.InterruptionType;
 import com.wbsedcl.trms.substation.log.domain.validation.ValidFeederId;
 import com.wbsedcl.trms.substation.log.domain.validation.ValidOfficeId;
 import com.wbsedcl.trms.substation.log.domain.validation.ValidUserId;
@@ -12,25 +10,17 @@ import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.List;
-
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class LogSourceChangeOverInterruptionCommand {
+public class LogMainPowerFailInterruptionCommand {
     @ValidFeederId
-    private String sourceChangeOverFromFeederId;
-    @ValidFeederId
-    private String sourceChangeOverToFeederId;
-    @ValidOfficeId(message = "Given substation office-id is invalid")
+    private String failed33kVSourceFeederId;
+    @ValidOfficeId
     private String substationOfficeId;
-    @NotNull
-    private InterruptionType interruptionType;
-    @NotNull
-    private List<String> affectedPTRIds;
     @ValidUserId(message="Created-by user-id is invalid")
     private String createdByUserId;
     @NotNull
@@ -43,8 +33,11 @@ public class LogSourceChangeOverInterruptionCommand {
     @NotNull
     private InterruptionStatus interruptionStatus;
     private String cause;
+    @NotNull
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+    @NotNull
     @JsonFormat(pattern = "HH:mm")
     private LocalTime endTime;
+
 }
